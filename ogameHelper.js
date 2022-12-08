07bd493bfe529acfcf85964b4119ed774d0fc4c3
Matthijn Zwart <matthijn.zwart@divide.nl>
2022-12-08 13:25:44 +0100

    created new version 0.4

--- ogameHelper.js
+++ ogameHelper.js
@@ -808,8 +808,7 @@ class OgameHelper {
     }
 
     getAmountOfExpeditionSlots(){
-        return 7;
-        //TODO: Get actual amount of exposlots
+        return Math.floor(Math.sqrt(parseInt(this.json.player.astro))) + (this.json.player.playerClass == PLAYER_CLASS_EXPLORER ? 2 : 0) + (this.json.player.admiral ? 1 : 0);
     }
 
     getFactor(planet, productionType){
@@ -1482,7 +1481,6 @@ class OgameHelper {
 
         table.appendChild(tableBody);
         div.appendChild(table);
-        //TODO: CREATE ACCOUNT PRODUCTION
     }
 
     calcMinerBonusProfitHour(){
