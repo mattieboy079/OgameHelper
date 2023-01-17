1223aabd291901451f37bf970054ad770943ca8f
Matthijn Zwart <matthijn.zwart@divide.nl>
2023-01-17 17:41:51 +0100

    try starting to create function library

--- logic/researches.ts
+++ logic/researches.ts
@@ -1,3 +1,4 @@
+import { getLifeformLevelBonus } from "../functions";
 import { Planet } from "./planet";
 import { Upgradable } from "./upgradable";
 
@@ -17,6 +18,7 @@ export class Astrophysics extends Research {
         this.baseCrystalCost = 8000;
         this.baseDeutCost = 4000;
         this.resIncFactor = 1.75;
+        this.timeIncFactor = 1.75;
     }
 
     getProduction(level: number, planets: Planet[]): number[] {
@@ -31,7 +33,25 @@ export class PlasmaTechnology extends Research {
         this.baseCrystalCost = 4000;
         this.baseDeutCost = 1000;
         this.resIncFactor = 2;
+        this.timeIncFactor = 2;
     }
+
+    getCosts(level: number, planets: Planet[]): number[] {
+        let korting: number = 0;
+        
+        planets.forEach(p => {
+            const tech = p.lifeforms.techs.find(x => x.name == "Verbeterde Stellarator");
+            if(tech){
+                korting += tech.level * (1 + getLifeformLevelBonus(p.lifeforms.class))
+            }
+        });
+
+        const metalCost: number = Math.floor(this.baseMetalCost * Math.pow(this.resIncFactor, level));
+        const crystalCost: number = Math.floor(this.baseCrystalCost * Math.pow(this.resIncFactor, level));
+        const deutCost: number = Math.floor(this.baseDeutCost * Math.pow(this.resIncFactor, level));
+        return [metalCost, crystalCost, deutCost];
+    }
+
     
     getProduction(level: number, planets: Planet[]): number[] {
         return [0,0,0]; //TODO
